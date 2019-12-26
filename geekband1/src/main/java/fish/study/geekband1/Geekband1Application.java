878474a3f485f5255ea8dc190282267bb6be19d4
yuxiaozhuo <yuxiaozhuo@beyondsoft.com>
2019-12-26 10:58:27 +0800

    init

---
+++ geekband1/src/main/java/fish/study/geekband1/Geekband1Application.java
@@ -0,0 +1,13 @@
+package fish.study.geekband1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Geekband1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Geekband1Application.class, args);
+	}
+
+}
---
+++ geekband1/src/test/java/fish/study/geekband1/Geekband1ApplicationTests.java
@@ -0,0 +1,13 @@
+package fish.study.geekband1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Geekband1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
